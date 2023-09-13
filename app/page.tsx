4732bc55e29b96cfe74ba696861770ff2ca6a9b3
ekfman <ekfirstman@gmail.com>
2023-09-13 15:08:36 -0700

    initializing project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+//the same file as his index.tsx
+
+export default function Home() {
+  return (
+    <div className="text-3xl text-sky-500">
+      Hello World!
+    </div>
+  )
+}
